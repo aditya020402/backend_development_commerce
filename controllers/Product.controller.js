26bae9708887f73f9c46c9b781bf29e901500d36
aditya <aditya.as207@gmail.com>
2024-01-02 21:15:33 +0530

    completed product controller

--- controllers/Product.controller.js
+++ controllers/Product.controller.js
@@ -2,10 +2,52 @@ import Product from "../models/Product.model.js";
 import ErrorHandler from "../utils/errorhandler.js";
 import asyncError from '../middleware/catchAsyncError.js';
 import {uploadOnCloudinary,deleteOnCloudinary} from "../utils/cloudinary.js";
+import apiFeature from "../utils/apiFeature.js";
 
+//create product 
 
+const createProduct = asyncError(async(req,res,next)=>{
+    let images = [];
+    if(typeof req.body.images === "string"){
+        images.push(req.body.images);
+    }
+    else{
+        images = req.body.images;
+    }
+    const imagesLinks = [];
+    for(let i=0;i<images.length;i++){
+        const result = uploadOnCloudinary(images[i],"products");
+        imagesLinks.push({
+            public_id:result.public_id,
+            url:result.secure_url,
+        })
+    }
+    req.body.images = imagesLinks;
+    req.body.user = req.user.id;
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success:true,
+        product,
+    })
+})
 
-
+//get all products 
+const getAllProducts = asyncError(async(req,res,next)=>{
+    const resultPerPage = 8;
+    const productCount = await Product.countDocuments();
+    const apiFeature = new apiFeature(Product.find(),req.query).search().filter();
+    let products = await apiFeature.query;
+    let filteredProductsCount = products.length;
+    apiFeature.pagination(resultPerPage);
+    products = await apiFeature.query;
+    res.status(200).json({
+        success:true,
+        products,
+        productCount,
+        resultPerPage,
+        filteredProductsCount,
+    })
+})
 
 
 // get product details 
